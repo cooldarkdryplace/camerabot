471381d48b56c00982d0be45f68b4bde410c98df
Andres Zakharos <zakharov.andrii@gmail.com>
2017-02-22 19:16:49 +0200

    got rid of panics, enabled long polling

--- telegram/api.go
+++ telegram/api.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/binary"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -21,21 +22,30 @@ const (
 	methodSendPhoto      = "sendPhoto"
 	methodGetUpdates     = "getUpdates"
 	methodsendChatAction = "sendChatAction"
+
+	// Wait timeout for longpolling
+	timeout = 60
 )
 
 var token string
 
 func init() {
 	token = os.Getenv("TOKEN")
+	if token == "" {
+		log.Fatal("API token was not set as ENV variable with name: 'TOKEN'")
+	}
 }
 
-func GetUpdates(client connection.Client) ([]Update, error) {
+func GetUpdates(client connection.Client, lastMsgID int64) ([]Update, error) {
 	apiResponse := &UpdatesResponse{}
 
-	err := getJson(client, fmt.Sprintf("%s%s/%s", baseURL, token, methodGetUpdates), apiResponse)
-
+	err := getJson(
+		client,
+		fmt.Sprintf("%s%s/%s?timeout=%d&offset=%d", baseURL, token, methodGetUpdates, timeout, lastMsgID),
+		apiResponse,
+	)
 	if err != nil {
-		log.Print("Error getting updates", err)
+		log.Println("Error getting updates: ", err)
 		return nil, err
 	}
 
@@ -43,7 +53,7 @@ func GetUpdates(client connection.Client) ([]Update, error) {
 }
 
 func SendTextMessage(client connection.Client, chat int64, m string) {
-	log.Printf("Sending test message: %s to chat: %v", m, chat)
+	log.Printf("Sending text message: %q to chat: %v", m, chat)
 	client.Get(fmt.Sprintf("%s%s/%s?chat_id=%v&text=%s", baseURL, token, methodSendMessage, chat, m))
 }
 
@@ -53,57 +63,78 @@ func SendPicture(client connection.Client, chat int64, filename string) {
 
 	picture, err := os.Open(filename)
 	if err != nil {
-		log.Panic("error opening file")
+		log.Println("Error opening file: ", err)
+		return
 	}
 
 	defer picture.Close()
 
 	fileWriter, err := bodyWriter.CreateFormFile("photo", "img.png")
 	if err != nil {
-		log.Panic("error writing to buffer")
+		log.Println("Error writing to buffer: ", err)
+		return
 	}
 
 	_, err = io.Copy(fileWriter, picture)
 	if err != nil {
-		log.Panic("error reading file", err)
+		log.Println("Error copying file: ", err)
+		return
 	}
 
 	fieldWriter, err := bodyWriter.CreateFormField("chat_id")
 	if err != nil {
-		log.Panic("error writing chat_id to buffer")
+		log.Println("Error writing chat_id to buffer: ", err)
+		return
 	}
 
 	err = binary.Write(fieldWriter, binary.LittleEndian, chat)
 	if err != nil {
-		fmt.Println("binary.Write failed:", err)
+		fmt.Println("Failed to write data as binary to form field: ", err)
+		return
 	}
 
 	bodyWriter.Close()
 
-	req, err := http.NewRequest(http.MethodPost,
-		fmt.Sprintf("%s%s/%s?chat_id=%v", baseURL, token, methodSendPhoto, chat), bodyBuf)
+	req, err := http.NewRequest(
+		http.MethodPost,
+		fmt.Sprintf("%s%s/%s?chat_id=%v", baseURL, token, methodSendPhoto, chat),
+		bodyBuf,
+	)
 	if err != nil {
-		log.Panic(err)
+		log.Println("Failed to create POST request with picture: ", err)
+		return
 	}
 
 	req.Header.Set("Content-Type", bodyWriter.FormDataContentType())
 
 	res, err := client.Do(req)
 	if err != nil {
-		log.Panic(err)
+		log.Println("Error during POST to Telegram API: ", err)
+		return
 	}
 
 	if res.StatusCode != http.StatusOK {
-		log.Panic(fmt.Errorf("bad status: %s", res.Status))
+		log.Printf("HTTP status for API call was not OK: %s\n", res.Status)
 	}
 }
 
 func getJson(client connection.Client, url string, target interface{}) error {
 	r, err := client.Get(url)
 	if err != nil {
+		log.Printf("Tried to get conversation updates, error occured: %q\n", err)
 		return err
 	}
 	defer r.Body.Close()
 
-	return json.NewDecoder(r.Body).Decode(target)
+	if r.StatusCode != http.StatusOK {
+		log.Printf("HTTP Get failed. Status code: %d\n", r.StatusCode)
+		return errors.New("get was not succesful")
+	}
+
+	if err = json.NewDecoder(r.Body).Decode(target); err != nil {
+		log.Println("Failed to unmarshal updates from JSON: %s", err)
+		return err
+	}
+
+	return nil
 }
