5d007ccd60ba08736a291e4e1f8772d133744647
Andres Zakharos <zakharov.andrii@gmail.com>
2017-02-28 12:30:38 +0200

    using lower resolution and high contrast for zoomed frame

--- updateFrame.sh
+++ updateFrame.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-/usr/bin/raspistill -n -q 98 -ex auto -hf -vf -t 1 -o /tmp/frame.jpg
-
+/usr/bin/raspistill -w 1024 -h 768 -n -q 80 -ex auto -hf -vf -t 1 -o /tmp/frame.jpg
--- updateZoomFrame.sh
+++ updateZoomFrame.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-/usr/bin/raspistill -n -q 98 --ISO 100 -ex auto -sh 100 -roi 0.593,0.52,0.04,0.04 -hf -vf -t 1 -o /tmp/zoomedFrame.png
+/usr/bin/raspistill -w 320 -h 240 \
+-mm spot -n -q 90 --ISO 100 -ex auto \
+-sa -100 -sh 100 -co 100 \
+-roi 0.593,0.52,0.04,0.04 \
+-hf -vf -t 1 \
+-o /tmp/zoomedFrame.jpg
